03e96a9d00c28f3c0c23542a8ec86c4cc2e29e42
antimont <128886920+antimont@users.noreply.github.com>
2024-07-16 23:16:57 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'thdatnguyen715.cxsygcw.mongodb.net',
+    DB_USER: 'thdatnguyen715',
+    DB_PASS: 'Abc123',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'a',
+    JWT_EXPIRES: '360000000', // in milliseconds
+    EMAIL_USER: 'thanhdatnguyen715@gmail.com', // gmail service
+    EMAIL_PASS: 'jzep jmnu owwz dfqv'
+  };
+  module.exports = MyConstants;
